f100429d66a554e6a5b65a126ac51ac4580c4ef4
DhineshrajBaskaran <dhineshrajbaskaran01@gmail.com>
2024-09-02 22:02:56 +0530

    this is the day1 class

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const {MongoClient}=require('mongodb');
 
-const uri=
+const uri=`mongodb+srv://dhineshrajbaskaran01:010995Dj@guvizen.0wfesxi.mongodb.net/?retryWrites=true&w=majority&appName=Guvizen`;
+
+const client=new MongoClient(uri);
+
+
+try{
+    client.connect(()=>{
+        console.log("connected to mongodb")
+    })
+}catch(e){
+    console.log("error while connecting the mongodb")
+};
